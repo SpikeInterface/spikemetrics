01d9dd834f30073be76c4bea7d2b6aa6b8895c41
colehurwitz <colehurwitz@gmail.com>
2020-04-19 14:06:49 -0700

    added duration rather than min and max time

--- spikemetrics/metrics.py
+++ spikemetrics/metrics.py
@@ -147,7 +147,7 @@ def calculate_metrics(spike_times, spike_clusters, amplitudes, pc_features, pc_f
 
 # ===============================================================
 
-def calculate_isi_violations(spike_times, spike_clusters, total_units, isi_threshold, min_isi, verbose=True):
+def calculate_isi_violations(spike_times, spike_clusters, total_units, isi_threshold, min_isi, duration, verbose=True):
     cluster_ids = np.unique(spike_clusters)
 
     viol_rates = np.zeros((total_units,))
@@ -159,15 +159,14 @@ def calculate_isi_violations(spike_times, spike_clusters, total_units, isi_thres
 
         for_this_cluster = (spike_clusters == cluster_id)
         viol_rates[cluster_id], num_violations = isi_violations(spike_times[for_this_cluster],
-                                                                min_time=np.min(spike_times),
-                                                                max_time=np.max(spike_times),
+                                                                duration=duration,
                                                                 isi_threshold=isi_threshold,
                                                                 min_isi=min_isi)
 
     return viol_rates
 
 
-def calculate_presence_ratio(spike_times, spike_clusters, total_units, verbose=True):
+def calculate_presence_ratio(spike_times, spike_clusters, total_units, duration, verbose=True):
     cluster_ids = np.unique(spike_clusters)
 
     ratios = np.zeros((total_units,))
@@ -179,20 +178,16 @@ def calculate_presence_ratio(spike_times, spike_clusters, total_units, verbose=T
 
         for_this_cluster = (spike_clusters == cluster_id)
         ratios[cluster_id] = presence_ratio(spike_times[for_this_cluster],
-                                            min_time=np.min(spike_times),
-                                            max_time=np.max(spike_times))
+                                            duration=duration)
 
     return ratios
 
 
-def calculate_firing_rate_and_spikes(spike_times, spike_clusters, total_units, verbose=True):
+def calculate_firing_rate_and_spikes(spike_times, spike_clusters, total_units, duration, verbose=True):
     cluster_ids = np.unique(spike_clusters)
 
     firing_rates = np.zeros((total_units,))
     num_spikes = np.zeros((total_units,))
-
-    min_time = np.min(spike_times)
-    max_time = np.max(spike_times)
 
     for idx, cluster_id in enumerate(cluster_ids):
 
@@ -201,8 +196,7 @@ def calculate_firing_rate_and_spikes(spike_times, spike_clusters, total_units, v
 
         for_this_cluster = (spike_clusters == cluster_id)
         firing_rates[cluster_id] = firing_rate(spike_times[for_this_cluster],
-                                               min_time=np.min(spike_times),
-                                               max_time=np.max(spike_times))
+                                               duration=duration)
         num_spikes[cluster_id] = len(spike_times[for_this_cluster])
 
     return firing_rates, num_spikes
@@ -453,7 +447,7 @@ def calculate_drift_metrics(spike_times,
 # ==========================================================
 
 
-def isi_violations(spike_train, min_time, max_time, isi_threshold, min_isi=None):
+def isi_violations(spike_train, duration, isi_threshold, min_isi=None):
     """Calculate Inter-Spike Interval (ISI) violations for a spike train.
 
     Based on metric described in Hill et al. (2011) J Neurosci 31: 8699-8705
@@ -464,8 +458,7 @@ def isi_violations(spike_train, min_time, max_time, isi_threshold, min_isi=None)
     Inputs:
     -------
     spike_train : array of monotonically increasing spike times (in seconds) [t1, t2, t3, ...]
-    min_time : start of recording (in seconds)
-    max_time : end of recording (in seconds)
+    duration : length of recording (seconds)
     isi_threshold : threshold for classifying adjacent spikes as an ISI violation
       - this is the biophysical refractory period
     min_isi : minimum possible inter-spike interval (default = None)
@@ -492,21 +485,20 @@ def isi_violations(spike_train, min_time, max_time, isi_threshold, min_isi=None)
     num_spikes = len(spike_train)
     num_violations = sum(isis < isi_threshold)
     violation_time = 2 * num_spikes * (isi_threshold - min_isi)
-    total_rate = firing_rate(spike_train, min_time, max_time)
+    total_rate = firing_rate(spike_train, duration)
     violation_rate = num_violations / violation_time
     fpRate = violation_rate / total_rate
 
     return fpRate, num_violations
 
 
-def presence_ratio(spike_train, min_time, max_time, num_bin_edges=101):
+def presence_ratio(spike_train, duration, num_bin_edges=101):
     """Calculate fraction of time the unit is present within an epoch.
 
     Inputs:
     -------
     spike_train : array of spike times
-    min_time : minimum time for potential spikes
-    max_time : maximum time for potential spikes
+    duration : length of recording (seconds)
     num_bin_edges : number of bin edges for histogram
       - total bins = num_bin_edges - 1
 
@@ -516,12 +508,12 @@ def presence_ratio(spike_train, min_time, max_time, num_bin_edges=101):
 
     """
 
-    h, b = np.histogram(spike_train, np.linspace(min_time, max_time, num_bin_edges))
+    h, b = np.histogram(spike_train, np.linspace(0, duration, num_bin_edges))
 
     return np.sum(h > 0) / (num_bin_edges - 1)
 
 
-def firing_rate(spike_train, min_time=None, max_time=None):
+def firing_rate(spike_train, duration):
     """Calculate firing rate for a spike train.
 
     If either temporal bound is not specified, the first and last spike time are used by default.
@@ -529,8 +521,7 @@ def firing_rate(spike_train, min_time=None, max_time=None):
     Inputs:
     -------
     spike_train : array of spike times (in seconds)
-    min_time : time of first possible spike (optional)
-    max_time : time of last possible spike (optional)
+    duration : length of recording (in seconds)
 
     Outputs:
     --------
@@ -538,14 +529,6 @@ def firing_rate(spike_train, min_time=None, max_time=None):
         Firing rate in Hz
 
     """
-
-    if min_time is None:
-        min_time = np.min(spike_train)
-
-    if max_time is None:
-        max_time = np.max(spike_train)
-
-    duration = max_time - min_time
     fr = spike_train.size / duration
 
     return fr
